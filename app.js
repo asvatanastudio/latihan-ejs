3dd1b16324299f7e608db65165144443bf0667b4
asvatanastudio <asvatanaplayground@gmail.com>
2025-09-21 21:53:32 +0700

    menambahkan layout

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const port = 3000;
 
 // Gunakan EJS
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(expressLayouts);
-app.set('layout', 'layouts/main-layout');
-app.use(express.static("public"));
+app.set("layout", "layouts/main-layout");
 
 // Data dummy
 const mahasiswa = [
-  { nama: 'Ade', email: 'ade@gmail.com' },
-  { nama: 'Asep', email: 'asep@gmail.com' },
-  { nama: 'Agus', email: 'agus@gmail.com' },
+  { nama: "Ade", email: "ade@gmail.com" },
+  { nama: "Asep", email: "asep@gmail.com" },
+  { nama: "Agus", email: "agus@gmail.com" },
 ];
 
 // Routing
-app.get('/', (req, res) => {
-  res.render('index', {
-    nama: 'Deden',
-    title: 'Halaman Home',
+app.get("/", (req, res) => {
+  res.render("index", {
+    nama: "deden",
+    title: "Halaman Home",
     mahasiswa,
   });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'Halaman About' });
+app.get("/about", (req, res) => {
+  res.render("about", { title: "Halaman About" });
 });
 
-app.get('/contact', (req, res) => {
-  res.render('contact', { title: 'Halaman Contact' });
+app.get("/contact", (req, res) => {
+  res.render("contact", { title: "Halaman Contact" });
 });
 
 // Middleware 404
-app.use('/', (req, res) => {
+app.use("/", (req, res) => {
   res.status(404);
-  res.send('<h1>404 - Page Not Found</h1>');
+  res.send("<h1>404 - Page Not Found</h1>");
 });
 
 app.listen(port, () => {
